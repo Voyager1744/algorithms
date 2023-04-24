0680d79301c93682311caada3af69af472712abc
Dmitriy Ushakov Air <udv8@ya.ru>
2023-04-25 00:53:09 +0300

    Фибоначи и дерево

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+'''
+Возвращает n элемент последовательности Фибоначи.
+'''
+
+def ith_fibonacci(n):
+    if n <= 1:
+        return n
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+        # print(current)
+    return current
+
+print(ith_fibonacci(10))
---
+++ tree_DFS.py
@@ -0,0 +1,41 @@
+"""
+Задача. Дано бинарное дерево, нужно вывести список списков значений
+вершин «по слоям». В каждом слое значения должны идти слева направо.
+Для дерева:
+     5
+    / \
+   /   \
+  3     1
+   \   /
+    4 2
+нужно вернуть
+[[5], [3, 1], [4, 2]]
+
+"""
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Banch(dict):
+    def __init__(self, *args, **kwargs):
+        super(Banch, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+def get_layered_representation(root):
+    result = []
+    DFS(root, 0, result)
+    return result
+
+def DFS(node, depth, result):
+    if not node:
+        return
+    # Т.к. мы выбрали preorder, то результат нужно увеличивать
+    # не больше, чем на 1
+    if depth >= len(result):
+        result.append([])
+    result[depth].append(node.val)
+    DFS(node.left, depth + 1, result)
+    DFS(node.right, depth + 1, result)
